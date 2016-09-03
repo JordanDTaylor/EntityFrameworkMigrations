6d6daee213cfb669f5b9b4730bb9c9316dd3dbb9
Jordan Taylor <jordandtaylor@gmail.com>
2016-09-03 00:17:45 -0600

    Removed the pluralizing naming convention (with migration)

--- Cintera.DAL/CinteraContext.cs
+++ Cintera.DAL/CinteraContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,12 @@ namespace Cintera.DAL
         }
 
         public DbSet<Case> Cases { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions
+                .Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
