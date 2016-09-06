28232462f8bd7fd7b6aaceffa20ec3b2c03ea8e2
Jordan Taylor <jordandtaylor@gmail.com>
2016-09-05 23:17:21 -0600

    Added Sightings

--- Cintera.DAL/Case.cs
+++ Cintera.DAL/Case.cs
@@ -9,5 +9,18 @@ namespace Cintera.DAL
     public class Case
     {
         public int CaseId { get; set; }
+
+        public virtual ICollection<Sighting> Signtings { get; set; }
+    }
+
+    public class Sighting
+    {
+        public int SightingId { get; set; }
+        public int CaseId { get; set; }
+        public virtual Case Case { get; set; }
+
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+
     }
 }
